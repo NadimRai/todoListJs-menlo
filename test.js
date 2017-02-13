caa0836c8301229d3d9ddd3f5ac417ec0030684d
NadimRai <rai_nadim@yahoo.com>
2017-02-12 23:57:20 -0800

    change color and textDecoration on li click

---
+++ test.js
@@ -0,0 +1,50 @@
+// $('li').click(function(){
+// 	alert("Clicked li");
+// })
+
+// // turns list red
+// $('li').click(function(){
+// 	$(this).css("color","red");
+// });
+
+// // strike line-through
+// $('li').click(function(){
+// 	$(this).css("color","red");
+// 	$(this).css("text-decoration", "line-through");
+// });
+
+// // strike line-through in single line -- watch out for text-decoration
+// $('li').click(function(){
+// 	$(this).css({
+// 		color: "red",
+// 		textDecoration: "line-through"
+// 	});
+// });
+
+// // strike line-through 
+// $('li').click(function(){
+// 	console.log($(this).css("color"));
+// 	if($(this).css("color") === "rgb(255, 0, 0)"){
+// 		console.log("it is currently red");
+// 	}
+// 	$(this).css({
+// 		color: "red",
+// 		textDecoration: "line-through"
+// 	});
+// });
+
+// strike line-through toggle color
+$('li').click(function(){
+	console.log($(this).css("color"));
+	if($(this).css("color") === "rgb(255, 0, 0)"){
+		$(this).css({
+			color: "black",
+			textDecoration: "none"
+		});
+	}else{
+		$(this).css({
+			color: "red",
+			textDecoration: "line-through"
+		});
+	}
+});
